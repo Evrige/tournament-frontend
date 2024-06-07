8a4506333a877e64d773482fa3390705cc93989b
Evrige <artur.dudnik@inbox.ru>
2024-06-07 09:28:54 +0300

    FIX: Logo

--- src/app/components/UI/TeamUserLogo.tsx
+++ src/app/components/UI/TeamUserLogo.tsx
@@ -4,12 +4,13 @@ import { getImageUrl } from '@/app/utils/getImageUrl'
 
 const TeamUserLogo = ({url, alt, size}: {url: string, alt: string, size?: string}) => {
 	if (url === "") return <div></div>
+
 	return (
-		<div className={`${size ? `w-${size} h-${size}` : "w-10 h-10"} rounded-full overflow-hidden flex items-center justify-center`}>
-			{url ? 	<Image src={url.startsWith('http') ? url : getImageUrl(url)}
+		<div className={`${size ? size : "w-10 h-10" } rounded-full overflow-hidden flex items-center justify-center`}>
+			<Image src={url.startsWith('http') ? url : getImageUrl(url)}
 										 width={200} height={200}
 										 className="w-full h-full object-cover"
-										 alt={alt} /> : ""}
+										 alt={alt} />
 		</div>
 	)
 }
